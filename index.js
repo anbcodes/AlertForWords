ff473a551c6667fe3b5143eefe0329860a54cf80
Anbcodes <andrewbaumes@gmail.com>
2019-06-20 10:23:17 -0400

    change when it gets contents of the webpack

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-let text = document.body.innerText
-let textToFind = ""
 chrome.storage.sync.get({
     text: '',
     time: 1000
   }, (items) => {
-    textToFind = items.text;
+    let textToFind = items.text;
     let regexStr = "("
     textToFind.split("\n").forEach((word, index) => {
         if (index != textToFind.split("\n").length-1) {
@@ -17,6 +15,7 @@ chrome.storage.sync.get({
 
     let regex = new RegExp(regexStr, "gi")
     setTimeout(() => {
+        let text = document.body.innerText
         let found = text.match(regex)
         if (found) {
             alert(`Found Word`)
