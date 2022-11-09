794725e4a15490bcb236038fe8f093b0baf79028
Anbcodes <me@anb.codes>
2022-11-08 22:08:35 -0500

    added sound to alert (version 1.2.0) fixes #2

--- options.js
+++ options.js
@@ -1,14 +1,20 @@
 function addItemToDiv(div, value, kind) {
     let itemDiv = document.createElement("div")
+
     itemDiv.className = "itemDiv"
+
     let inputText = document.createElement("input")
+
     inputText.type = "text"
     inputText.value = value
     inputText.className = "inputText"
     itemDiv.appendChild(inputText)
+
     let inputButton = document.createElement("button")
+
     inputButton.innerText = kind
     inputButton.className = kind
+
     inputButton.onclick = (e) => {
         if (e.target.innerText === "text") {
             e.target.innerText = "regex"
@@ -18,22 +24,29 @@ function addItemToDiv(div, value, kind) {
             e.target.className = "text"
         }
     }
+
     let removeButton = document.createElement("button")
+
     removeButton.className = "remove"
     removeButton.innerText = "-"
     removeButton.style.margin = "5px"
     removeButton.onclick = (e) => {
         e.target.parentElement.remove()
     }
+
     itemDiv.appendChild(inputButton)
     itemDiv.appendChild(removeButton)
     div.appendChild(itemDiv)
 }
+
 let doneButton = document.getElementById("submit")
+
 chrome.storage.sync.get({
     storage: [],
     time: 1000,
-    interval: 0
+    interval: 0,
+    playSound: false,
+    alert: true,
   }, function(items) {
     let inputDiv = document.getElementById("input")
     items.storage.forEach(item => {
@@ -41,12 +54,15 @@ chrome.storage.sync.get({
     });
     document.getElementById('time').value = items.time
     document.getElementById('interval').value = items.interval
+    document.getElementById('sound').checked = items.playSound
+    document.getElementById('alert').checked = items.alert
     let addButton = document.getElementById("addButton")
     addButton.className = "add"
     addButton.onclick = () => {
         addItemToDiv(inputDiv, "", "text")
     }
 });
+
 doneButton.onclick = () => {
     let inputs = Array.from(document.getElementById("input").children)
     let storage = []
@@ -55,11 +71,15 @@ doneButton.onclick = () => {
     })
     let time = document.getElementById('time').value
     let interval = document.getElementById('interval').value
+    let playSound = document.getElementById('sound').checked
+    let alert = document.getElementById('alert').checked
 
     chrome.storage.sync.set({
         storage: storage,
         time: time,
-        interval: interval
+        interval: interval,
+        playSound: playSound,
+        alert: alert,
     }, function() {
         var status = document.getElementById('status');
         status.textContent = 'Options saved.';
